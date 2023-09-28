c01f8a924088a6ca88e569af65266e7d2b72e2e1
jonathan <jonathancolacocarr@gmail.com>
2023-09-27 23:34:40 -0400

    Add models and optimizers

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import numpy as np
-from data_processing.data_processing import  fetch_housing_dataset, fetch_wine_dataset, housing_tt_split
+from data_processing.data_processing import  fetch_housing_dataset, fetch_wine_dataset, housing_tt_split, wine_tt_split
 from data_analysis.data_analysis import dataframe_statistics
 np.random.seed(0)
 
 housing_df = fetch_housing_dataset()
 wine_df = fetch_wine_dataset()
 
-# analyze the data
-# dataframe_statistics(housing_df, plot_hist=True, fig_filepath="./out/figures/housing_df_features.png")
-# dataframe_statistics(wine_df, plot_hist=True, fig_filepath="./out/figures/wine_df_features.png")
-
 # 2.1 Analytic logistic regression
-X_housing_train, X_housing_test, y_housing_train, y_housing_test = housing_tt_split(housing_df, test_size=0.25)
+#X_housing_train, X_housing_test, y_housing_train, y_housing_test = housing_tt_split(housing_df, test_size=0.25)
 
-analytic_lin_reg = AnalyticLinearRegression()
-analytic_lin_reg.fit(X_housing_train, y_housing_train)
-preds = analytic_lin_reg.predict(X_housing_test)
+# 2.2 Logistic regression with SGD. Achieves about 91% accuracy on test set with default parameters.
+X_wine_train, X_wine_test, y_wine_train, y_wine_test = wine_tt_split(wine_df, test_size=0.25)
+log_r = LogisticRegression()
+log_r.fit(X_wine_train, y_wine_train, verbose=True, batch_size=X_wine_train.shape[0])
+y_preds = log_r.predict(X_wine_test)
+y_preds_binary = (y_preds >= y_preds.max(axis=1, keepdims=True)).astype(int)
+print(accuracy_score(y_wine_test, y_preds_binary))
 
-ls_loss = 0.5 * np.dot(np.transpose(y_housing_test - preds), y_housing_test - preds)
-print('Least squares loss for analytic linear regression: ', ls_loss)
+# 2.3 Linear regression with gradient descent on dummy data
+#N = 2000
+#D = 13
+#X = np.random.rand(N, D)
+#w_star = np.random.rand(D, 1)
+#y = np.dot(X, w_star)
+#print(y.shape)
+#
+#lin_reg = LinearRegression(add_bias=False)
+#lin_reg.fit(X, y)
---
+++ models/optimizers.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+class StochasticGradientDescent:
+    """
+    Stochastic gradient descent with momentum. Reduces to standard gradient descent when :batch_size: is equal to the
+    number of samples in the training set. The parameter :beta: controls the momentum. When :beta: is equal to zero,
+    the optimizer runs normal stochastic gradient descent.
+    """
+
+    def __init__(self, learning_rate=.1, max_iters=1e4, epsilon=1e-8, batch_size=1, record_history=False, verbose=True,
+                 beta=0):
+        self.learning_rate = learning_rate
+        self.max_iters = max_iters
+        self.record_history = record_history
+        self.epsilon = epsilon
+        self.verbose = verbose
+        self.batch_size = batch_size
+        self.beta = beta    # momentum
+        if record_history:
+            self.w_history = []  # to store the weight history for visualization
+
+    def run(self, gradient_fn, X, y, w):
+        assert self.batch_size <= X.shape[0], f'Error, batch size must be smaller than {X.shape[0]}'
+        ix_list = [i for i in range(X.shape[0])]    # possible indices for each mini batch
+
+        grad = np.inf
+        prev_delta_w = np.zeros(w.shape)
+        t = 0
+        while np.linalg.norm(grad) > self.epsilon and t < self.max_iters:
+
+            # update weights according to the equation in slide 30 of:
+            # https://www.cs.mcgill.ca/~isabeau/COMP551/F23/slides/5-gradientdescent.pdf
+            batch = np.random.choice(ix_list, size=self.batch_size, replace=False)
+
+            grad = gradient_fn(X[batch], y[batch], w)
+
+            delta_w = self.beta * prev_delta_w + (1-self.beta) * grad
+            prev_delta_w = delta_w
+
+            w = w - self.learning_rate * delta_w
+
+            if self.verbose and (t % 100 == 0):
+                print(f'gradient norm at step {t}: {np.linalg.norm(grad)}')
+
+            if self.record_history:
+                self.w_history.append(w)
+            t += 1
+        return w
+
+
+class Adam:
+    """
+    Implements the Adam gradient descent technique according to the weight update in slide 35 of:
+    https://www.cs.mcgill.ca/~isabeau/COMP551/F23/slides/5-gradientdescent.pdf
+    """
+
+    def __init__(self, learning_rate=.1, max_iters=1e4, epsilon=1e-8, batch_size=1, record_history=False, verbose=True,
+                 beta_1=0, beta_2=0):
+        self.learning_rate = learning_rate
+        self.max_iters = max_iters
+        self.record_history = record_history
+        self.epsilon = epsilon
+        self.verbose = verbose
+        self.batch_size = batch_size
+        self.beta_1 = beta_1    # used for moving average of the first moment
+        self.beta_2 = beta_2    # used for moving average of the second moment
+        if record_history:
+            self.w_history = []  # to store the weight history for visualization
+
+    def run(self, gradient_fn, X, y, w):
+        assert self.batch_size <= X.shape[0], f'Error, batch size must be smaller than {X.shape[0]}'
+        ix_list = [i for i in range(X.shape[0])]    # possible indices for each mini batch
+
+        grad = np.inf
+        prev_M = np.zeros(w.shape)
+        prev_S = np.zeros(w.shape)
+        beta_1_t = 1
+        beta_2_t = 1
+
+        t = 0
+        while np.linalg.norm(grad) > self.epsilon and t < self.max_iters:
+
+            batch = np.random.choice(ix_list, size=self.batch_size, replace=False)
+
+            grad = gradient_fn(X[batch], y[batch], w)
+
+            # Compute weighted moving average of first and second moment of the cost gradient
+            M = self.beta_1 * prev_M + (1-self.beta_1) * grad
+            S = self.beta_2 * prev_S + (1-self.beta_2) * grad**2
+            prev_M = M
+            prev_S = S
+
+            beta_1_t *= self.beta_1
+            beta_2_t *= self.beta_2
+            M_hat = M / (1 - beta_1_t)
+            S_hat = S / (1 - beta_2_t)
+
+            w = w - (self.learning_rate * M_hat / np.sqrt(S_hat + self.epsilon))
+
+            if self.verbose and (t % 100 == 0):
+                print(f'gradient norm at step {t}: {np.linalg.norm(grad)}')
+
+            if self.record_history:
+                self.w_history.append(w)
+
+            t += 1
+        return w

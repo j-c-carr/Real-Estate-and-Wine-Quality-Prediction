eae96986159d359c08b0183049ba674a9db1caee
jonathan <jonathancolacocarr@gmail.com>
2023-09-30 10:01:21 -0400

    Add one hot encoding to Logistic Regression predictor

--- main.py
+++ main.py
@@ -15,19 +15,18 @@ housing_df = fetch_housing_dataset(preprocess=True)
 
 # 2.2 Logistic regression with SGD. Achieves about 91% accuracy on test set with default parameters.
 wine_df = fetch_wine_dataset()
-#one_hot_wine_classes = pd.get_dummies(wine_df['class']).to_numpy(dtype=int)
-#X_wine_train, X_wine_test, y_wine_train, y_wine_test = train_test_split(wine_df.drop(['class'], axis=1),
-#                                                                        one_hot_wine_classes,
-#                                                                        test_size=0.25, stratify=wine_df['class'])
+one_hot_wine_classes = pd.get_dummies(wine_df['class']).to_numpy(dtype=int)
+X_wine_train, X_wine_test, y_wine_train, y_wine_test = train_test_split(wine_df.drop(['class'], axis=1),
+                                                                        one_hot_wine_classes,
+                                                                        test_size=0.25, stratify=wine_df['class'])
 
-# log_r = LogisticRegression()
-# log_r.fit(X_wine_train, y_wine_train, optimizer_class=Adam, max_iters=1e4, verbose=True, batch_size=X_wine_train.shape[0])
-# y_preds = log_r.predict(X_wine_test)
-# y_preds_bin = (y_preds >= y_preds.max(axis=1, keepdims=True)).astype(int)
-# print('sklean accuracy: ', accuracy_score(y_wine_test, y_preds_bin))
-# print('sklean precision: ', precision_score(y_wine_test, y_preds_bin, average='weighted'))
-# print('sklean recall: ', recall_score(y_wine_test, y_preds_bin, average='weighted'))
-# print('sklean f1: ', f1_score(y_wine_test, y_preds_bin, average='weighted'))
+log_r = LogisticRegression()
+log_r.fit(X_wine_train, y_wine_train, optimizer_class=Adam, max_iters=1e4, verbose=True, batch_size=X_wine_train.shape[0])
+y_preds = log_r.predict(X_wine_test)
+print('sklean accuracy: ', accuracy_score(y_wine_test, y_preds))
+print('sklean precision: ', precision_score(y_wine_test, y_preds, average='weighted'))
+print('sklean recall: ', recall_score(y_wine_test, y_preds, average='weighted'))
+print('sklean f1: ', f1_score(y_wine_test, y_preds, average='weighted'))
 
 # 2.3 Linear regression with gradient descent
 X_housing_train, X_housing_test, y_housing_train, y_housing_test = train_test_split(housing_df.drop(['MEDV'], axis=1).to_numpy(),

c9c57bd1b18eaf32a6afe72bbbff1c2dc0e0e155
jantiegges <jantiegges@gmail.com>
2023-10-02 13:07:37 -0400

    modify experiments

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import numpy as np
-
 import pandas as pd
 
-from data_acquisition.data_acquisition import  fetch_housing_dataset, fetch_wine_dataset
+from utils.data_acquisition import  fetch_housing_dataset, fetch_wine_dataset
 from models.models import LinearRegression, LogisticRegression
 from models.optimizers import GradientDescent, StochasticGradientDescent, Adam
 from utils.metrics import mse
 from utils.data_preparation import custom_train_test_split
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-np.random.seed(0)
 
+np.random.seed(0)
 
 
 # 2.2 Logistic regression with SGD. Achieves about 91% accuracy on test set with default parameters.
@@ -17,7 +16,8 @@ wine_df = fetch_wine_dataset()
 one_hot_wine_classes = pd.get_dummies(wine_df['class']).to_numpy(dtype=int)
 X_wine_train, X_wine_test, y_wine_train, y_wine_test = custom_train_test_split(wine_df.drop(['class'], axis=1).to_numpy(),
                                                                         one_hot_wine_classes,
-                                                                        test_size=0.20, stratify=wine_df['class'])
+                                                                        test_size=0.20, stratify=wine_df['class'],
+                                                                        random_seed=0)
 
 log_r = LogisticRegression()
 log_r.fit(X_wine_train, y_wine_train, optimizer_class=GradientDescent, max_iters=4e4, learning_rate=0.05, verbose=False)
@@ -43,9 +43,10 @@ print(f'F1 score\n\tGD: {gd_f1_score}, SGD with mini-batch of size {batch_size}:
 
 # exit()
 # 2.3 Linear regression with gradient descent
-housing_df = fetch_housing_dataset(preprocess=True)
+housing_df = fetch_housing_dataset()
 X_housing_train, X_housing_test, y_housing_train, y_housing_test = custom_train_test_split(housing_df.drop(['MEDV'], axis=1).to_numpy(),
-                                                                                    housing_df.MEDV.to_numpy().reshape(-1,1), test_size=0.20)
+                                                                                    housing_df.MEDV.to_numpy().reshape(-1,1), test_size=0.20,
+                                                                                    random_seed=0)
 lin_reg = LinearRegression(add_bias=True)
 w_star = lin_reg.fit(X_housing_train, y_housing_train, analytic_fit=True)
 lin_reg.fit(X_housing_train, y_housing_train, optimizer_class=StochasticGradientDescent, batch_size=X_housing_train.shape[0], record_history=True, beta=0)
